d64fb532ac260b82fdedee40f014a82f7e190129
BearAzul <adyaabduazizulhakim@gmail.com>
2024-09-22 15:21:36 +0700

    filter

--- src/pages/admin/ProductsView.jsx
+++ src/pages/admin/ProductsView.jsx
@@ -1,10 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { Link, useLoaderData, useNavigation } from "react-router-dom";
+import { Link, useLoaderData, useNavigation, Form } from "react-router-dom";
 import customAPI from "../../api.js";
 import { CardProductAdmin } from "../../components/CardProduct.jsx";
-import Categories from "../../assets/data/Category.jsx";
 import Loading from "../../components/Loading.jsx";
 import "../../styles/index.css"
+import { FormSelect } from "../../components/FormInput.jsx";
 
 export const loader = async ({ request }) => {
   const params = Object.fromEntries([
@@ -18,7 +18,9 @@ export const loader = async ({ request }) => {
 };
 
 const ProductsView = () => {
-  const { dataProducts } = useLoaderData();
+  const categories = ["Chair", "Bed", "Sofa", "Wardrobe", "Table", "Lamps"];
+  const { dataProducts, params } = useLoaderData();
+  const {name, category} = params
 
   const navigation = useNavigation();
   const isPageLoading = navigation.state === "loading";
@@ -32,33 +34,27 @@ const ProductsView = () => {
             <i className="ri-add-circle-line me-1"></i>
             Add New Product
           </Link>
-          <div className="d-flex align-items-center gap-1 justify-content-center flex-column flex-md-row">
-            <select
-              name="category"
-              id="category"
-              className="form-select form-select-sm w-100"
-            >
-              <option selected disabled>
-                -- Choose Category --
-              </option>
-              {Categories.map((category, index) => (
-                <option key={index} value={category.title}>
-                  {category.title}
-                </option>
-              ))}
-            </select>
+          <Form
+            method="get"
+            className="d-flex align-items-center gap-1 justify-content-center flex-wrap flex-md-nowrap"
+          >
+            <Link to="/admin/products" className="btn btn-sm btn-light">
+              <i className="ri-filter-off-line"></i>
+            </Link>
+            <FormSelect name="category" options={categories} defaultValue={category} />
             <div className="input-group">
               <input
                 name="search"
                 type="search"
                 placeholder="search"
+                defaultValue={name}
                 className="form-control form-control-sm"
               />
-              <button className="btn btn-warning btn-sm" type="button">
+              <button type="submit" className="btn btn-warning btn-sm">
                 <i className="ri-search-line"></i>
               </button>
             </div>
-          </div>
+          </Form>
         </div>
         <div className="mt-4 furniture__product">
           <Row lg="5" md="4" xs="2" className="g-2 g-lg-3">
