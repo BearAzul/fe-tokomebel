c7313ad7530fc08413b7d7a1f994fd19c526fdde
BearAzul <adyaabduazizulhakim@gmail.com>
2025-06-22 15:08:31 +0700

    add load in detail order

--- src/pages/admin/OrderDetailView.jsx
+++ src/pages/admin/OrderDetailView.jsx
@@ -7,6 +7,7 @@ import { formatToIDR } from "../../utils";
 import { DetailOrderDirect } from "../../components/Directlink.jsx";
 import BlankImages from "../../assets/Image/blank_user.png"
 import { HelmetHead } from "../../common/Helmet.jsx";
+import Loading from "../../components/Loading.jsx";
 
 const OrderDetailView = () => {
   const [detailOrder, setDetailOrder] = useState([]);
@@ -70,132 +71,138 @@ const OrderDetailView = () => {
       <section className="fm-2">
         <Container>
           <DetailOrderDirect />
-          <h6 className="mb-2">
-            ID Pesanan: <span className="text-warning">#{detailOrder._id}</span>
-          </h6>
-          <Row lg="2" className="g-3">
-            <Col lg="8">
-              <DataTable
-                data={detailOrder.itemsDetail}
-                columns={columns}
-                theme="dark"
-                className="rounded border border-secondary mb-3"
-              />
-              <Card className="border border-secondary text-bg-dark">
-                <Card.Body>
-                  <Card.Title className="border-bottom pb-2 border-secondary">
-                    Detail Pelanggan
-                  </Card.Title>
+          {detailOrder ? (
+            <>
+              <h6 className="mb-2">
+                ID Pesanan: <span className="text-warning">#{detailOrder._id}</span>
+              </h6>
+              <Row lg="2" className="g-3">
+                <Col lg="8">
+                  <DataTable
+                    data={detailOrder.itemsDetail}
+                    columns={columns}
+                    theme="dark"
+                    className="rounded border border-secondary mb-3"
+                  />
+                  <Card className="border border-secondary text-bg-dark">
+                    <Card.Body>
+                      <Card.Title className="border-bottom pb-2 border-secondary">
+                        Detail Pelanggan
+                      </Card.Title>
 
-                  <Table responsive className="table-dark">
-                    <tbody>
-                      <tr>
-                        <td>Nama Lengkap</td>
-                        <td>:</td>
-                        <td className="text-end">{`${detailOrder.firstName} ${detailOrder.lastName} `}</td>
-                      </tr>
-                      <tr>
-                        <td>No.Telp</td>
-                        <td>:</td>
-                        <td className="text-end">{detailOrder.phone}</td>
-                      </tr>
-                      <tr>
-                        <td>Email</td>
-                        <td>:</td>
-                        <td className="text-end">{detailOrder.email}</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col lg="4">
-              <Card className="text-bg-dark border border-secondary mb-3">
-                <Card.Body>
-                  <Card.Title className="border-bottom border-secondary pb-2">
-                    Status Pembayaran
-                  </Card.Title>
-                  <div className="d-flex gap-3">
-                    <figure
-                      style={{ width: "80px", height: "80px" }}
-                      className="overflow-hidden rounded border border-secondary m-0"
-                    >
-                      <img
-                        src={`${detailOrder.image === null
-                            ? BlankImages
-                            : detailOrder.image
-                          }`}
-                        alt={detailOrder.firstName}
-                        className="d-block w-100 h-100 object-fit-cover"
-                      />
-                    </figure>
-                    <div>
-                      <h6 className="mb-3">{`${detailOrder.firstName} ${detailOrder.lastName} `}</h6>
-                      <span
-                        className={`rounded py-1 px-2 ${detailOrder.status === "success"
-                            ? "text-bg-success"
-                            : detailOrder.status === "failed"
-                              ? "text-bg-danger"
-                              : "text-bg-warning"
-                          }`}
-                      >
-                        {detailOrder.status}
-                      </span>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-              <Card className="border border-secondary text-bg-dark mb-3">
-                <Card.Body>
-                  <Card.Title className="border-bottom border-secondary pb-2">
-                    Ringkasan
-                    <span className="text-bg-warning fs-7 px-2 py-1 rounded ms-2">
-                      Dalam Perjalanan
-                    </span>
-                  </Card.Title>
-                  <table className="w-100 fs-7">
-                    <tbody>
-                      <tr>
-                        <td>Pesanan Dibuat</td>
-                        <td>:</td>
-                        <td className="text-end">
-                          {new Date(detailOrder.createdAt).toLocaleString()}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Subtotal</td>
-                        <td>:</td>
-                        <td className="text-end">
-                          {formatToIDR(detailOrder.total - delivery)}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Ongkos Kirim</td>
-                        <td>:</td>
-                        <td className="text-end">{formatToIDR(delivery)}</td>
-                      </tr>
-                      <tr>
-                        <td>Total</td>
-                        <td>:</td>
-                        <td className="text-end">
-                          {formatToIDR(detailOrder.total)}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </Card.Body>
-              </Card>
-              <Card className="border border-secondary text-bg-dark">
-                <Card.Body>
-                  <Card.Title className="border-bottom pb-2 border-secondary">
-                    Alamat Pelanggan
-                  </Card.Title>
-                  <p>{detailOrder.city}</p>
-                  <p>{detailOrder.address}</p>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+                      <Table responsive className="table-dark">
+                        <tbody>
+                          <tr>
+                            <td>Nama Lengkap</td>
+                            <td>:</td>
+                            <td className="text-end">{`${detailOrder.firstName} ${detailOrder.lastName} `}</td>
+                          </tr>
+                          <tr>
+                            <td>No.Telp</td>
+                            <td>:</td>
+                            <td className="text-end">{detailOrder.phone}</td>
+                          </tr>
+                          <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td className="text-end">{detailOrder.email}</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col lg="4">
+                  <Card className="text-bg-dark border border-secondary mb-3">
+                    <Card.Body>
+                      <Card.Title className="border-bottom border-secondary pb-2">
+                        Status Pembayaran
+                      </Card.Title>
+                      <div className="d-flex gap-3">
+                        <figure
+                          style={{ width: "80px", height: "80px" }}
+                          className="overflow-hidden rounded border border-secondary m-0"
+                        >
+                          <img
+                            src={`${detailOrder.image === null
+                              ? BlankImages
+                              : detailOrder.image
+                              }`}
+                            alt={detailOrder.firstName}
+                            className="d-block w-100 h-100 object-fit-cover"
+                          />
+                        </figure>
+                        <div>
+                          <h6 className="mb-3">{`${detailOrder.firstName} ${detailOrder.lastName} `}</h6>
+                          <span
+                            className={`rounded py-1 px-2 ${detailOrder.status === "success"
+                              ? "text-bg-success"
+                              : detailOrder.status === "failed"
+                                ? "text-bg-danger"
+                                : "text-bg-warning"
+                              }`}
+                          >
+                            {detailOrder.status}
+                          </span>
+                        </div>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                  <Card className="border border-secondary text-bg-dark mb-3">
+                    <Card.Body>
+                      <Card.Title className="border-bottom border-secondary pb-2">
+                        Ringkasan
+                        <span className="text-bg-warning fs-7 px-2 py-1 rounded ms-2">
+                          Dalam Perjalanan
+                        </span>
+                      </Card.Title>
+                      <table className="w-100 fs-7">
+                        <tbody>
+                          <tr>
+                            <td>Pesanan Dibuat</td>
+                            <td>:</td>
+                            <td className="text-end">
+                              {new Date(detailOrder.createdAt).toLocaleString()}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Subtotal</td>
+                            <td>:</td>
+                            <td className="text-end">
+                              {formatToIDR(detailOrder.total - delivery)}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Ongkos Kirim</td>
+                            <td>:</td>
+                            <td className="text-end">{formatToIDR(delivery)}</td>
+                          </tr>
+                          <tr>
+                            <td>Total</td>
+                            <td>:</td>
+                            <td className="text-end">
+                              {formatToIDR(detailOrder.total)}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </Card.Body>
+                  </Card>
+                  <Card className="border border-secondary text-bg-dark">
+                    <Card.Body>
+                      <Card.Title className="border-bottom pb-2 border-secondary">
+                        Alamat Pelanggan
+                      </Card.Title>
+                      <p>{detailOrder.city}</p>
+                      <p>{detailOrder.address}</p>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </>
+          ) : (
+            <Loading />
+          )}
         </Container>
       </section>
     </>
