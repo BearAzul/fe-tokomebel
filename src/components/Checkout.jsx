27953956e45927ec840181cbcd24ccf1566e18b5
BearAzul <adyaabduazizulhakim@gmail.com>
2025-08-19 23:18:37 +0700

    fix

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -234,7 +234,7 @@ const Checkout = () => {
             </tbody>
           </table>
         </div>
-        {user && user.role !== "owner"(
+        {user && user.role !== "owner" && (
           <div className="mb-2 fs-7 input-group">
             {numItems ? (
               <button
