ef1f5c5dcbdb9b39fe2ceb9c328141866333cd91
BearAzul <adyaabduazizulhakim@gmail.com>
2025-10-14 06:31:22 +0700

    c

--- src/pages/maintenance/MaintenancePage.jsx
+++ src/pages/maintenance/MaintenancePage.jsx
@@ -9,7 +9,7 @@ const MaintenancePage = () => {
       <section id="maintenance">
         <Container className="d-flex align-items-center justify-content-center vh-100 fm-2">
           <div className="text-center p-1 wrapp">
-            <h1 className="fw-bold mb-1 ">🚧 Sedang dalam Pemeliharaan! 🚧</h1>
+            <h1 className="fw-bold mb-1 ">🚧 Sedang dalam Pemeliharaan 🚧</h1>
             <p className="mb-3">Maaf atas ketidaknyamanannya. Aplikasi kami sedang dalam tahap maintenance untuk mencegah kerusakan dan meningkatkan layanan kami.</p>
             <p className="text-muted mb-2">Silakan kunjungi kembali nanti.</p>
             <p className="fw-medium">&copy; {new Date().getFullYear()} Aplikasi Toko Mebel - AkimDev.</p>
